f35183cb71974e611cf4e466a2bb9b4f53f9186c
malithya_fernando <malithyafernando@gmail.com>
2020-05-25 19:44:38 +0530

    Question 1

---
+++ Assignment 2/question_1.scala
@@ -0,0 +1,12 @@
+object salaryObject{
+
+    def main(args: Array[String]){
+        println("Take home salary of the employee : Rs." + find_Salary(40, 20))
+    }
+
+    def find_Salary(Normal : Int, OT : Int) : Double = find_noTaxSalary(Normal, OT) - find_Tax(find_noTaxSalary(Normal, OT))
+
+    def find_noTaxSalary(Normal : Int, OT : Int) : Int = (150*Normal + 75*OT)*5
+
+    def find_Tax(Value : Int) : Double = Value*0.1
+}
