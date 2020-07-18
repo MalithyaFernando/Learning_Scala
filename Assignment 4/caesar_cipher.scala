ebf3a0f3f7ae79ec53b2c23309494e104c49e060
malithya_fernando <malithyafernando@gmail.com>
2020-07-18 17:47:10 +0530

    Update Caesar cipher

--- Assignment 4/caesar_cipher.scala
+++ Assignment 4/caesar_cipher.scala
@@ -5,25 +5,25 @@ object Ceasar_Cipher extends App{
 	val shift = (readLine("Shift By: ").toInt + alphabet.size) % alphabet.size
 	val msg = readLine("Secret Message: ")
 
-    val En = (c:Char, key:Int, a:String) => {
+	val E = (c:Char, key:Int, a:String) => {
 		if (a.indexOf(c.toUpper) == -1) c
 		else a((a.indexOf(c.toUpper) + key) % a.size)
-    }
+	}
 
-	val De = (c:Char, key:Int, a:String) => {
-        if (a.indexOf(c.toUpper) == -1) c
-        else{
-            if ((a.indexOf(c) - key)<0)	a(a.size - (a.indexOf(c) - key).abs % a.size)
+	val D = (c:Char, key:Int, a:String) => {
+		if (a.indexOf(c) == -1)	c
+		else{
+			if ((a.indexOf(c) - key)<0) a((a.indexOf(c) - key + a.size) % a.size)
 			else a((a.indexOf(c) - key) % a.size)
-        }
+		}
     }
     
-    val cipher = (algo: (Char, Int, String) => Char, s:String, key:Int, a:String) =>
-        s.map(algo(_, key, a))
+	val cipher = (algo: (Char, Int, String) => Char, s:String, key:Int, a:String) =>
+		s.map(algo(_, key, a))
 
-	val enm = cipher(En, msg, shift, alphabet)
-    println("Encrypted => " + enm);
+	val enm = cipher(E, msg, shift, alphabet)
+    println("Encypted => " + enm);
 
-    val dem = cipher(De, enm, shift, alphabet) 
-    println("Decrypted => " + dem);
+	val dem = cipher(D, enm, shift, alphabet) 
+	println("Decypted => " + dem);
 }
